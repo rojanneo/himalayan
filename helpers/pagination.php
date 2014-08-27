0f72ead1245d9a3fe22e44dd5ede228b18161419
rojanneo <rojan_neo@hotmail.com>
2014-08-27 11:43:20 +0545

    Pagination Design in work

--- helpers/pagination.php
+++ helpers/pagination.php
@@ -6,11 +6,10 @@ if(!function_exists('showPagination'))
 		$nextPage = $page + 1;
 		$prevPage = $page - 1;
 		if($prevPage != 0)
-			echo '<a href="'.URL.$urlkey.'?p='.$prevPage.'">Previous</a>';
+			echo '<a class="page dar gradient" href="'.URL.$urlkey.'?p='.$prevPage.'">Previous</a>';
 		for($i = 1; $i<=$pagination_num; $i++)
-			echo '<a href="'.URL.$urlkey.'?p='.$i.'">'.$i.'</a>';
+			echo '<a class="page dar gradient" href="'.URL.$urlkey.'?p='.$i.'">'.$i.'</a>';
 		if($nextPage <= $pagination_num)
-			echo '<a href="'.URL.$urlkey.'?p='.$nextPage.'">Next</a>';
-
+			echo '<a class="page dar gradient"href="'.URL.$urlkey.'?p='.$nextPage.'">Next</a>';
 	}
 }
